f42f4044516220d2cdc14e69afa20b1e3ee73146
Betty Waiyego <btynyambura20@gmail.com>
2024-06-20 14:11:40 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "programming is like building a multilingal puzzle, 
+ * followed by a new line"
+ * retirn o. 
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
