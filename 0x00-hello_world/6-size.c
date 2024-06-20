ebf9cfe52cf96bc8df15e729ed09a2efc187569d
Betty Waiyego <btynyambura20@gmail.com>
2024-06-20 15:13:41 -0700

    err

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - Prints the size of various types on the computer it is compiled and run on.
+ * main - Prints the size of various types on the computer it is
+ * compiled and run on.
  * Return: Always 0
  */
 
@@ -12,7 +13,7 @@ int main(void)
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
 
 	printf("size of an int: %zu byte(s)\n", sizeof(int));
-	printf("size of a long int: %zu byte(s)\n",sizeof(long int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a float %zu byte(s)\n", sizeof(float));
 	return (0);
 }
