c0fd5854ae29aeecd4ee4c76016b21944a44a9e2
Betty Waiyego <btynyambura20@gmail.com>
2024-06-22 06:48:41 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print decimal without char
+ * Return: always 0
+ */
+
+int main (void)
+{
+	int d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
+
