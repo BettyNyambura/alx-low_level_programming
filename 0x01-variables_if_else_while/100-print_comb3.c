1bb6295b7e9e964923afe9025ff2e953204c5d49
Betty Waiyego <btynyambura20@gmail.com>
2024-06-22 08:23:55 -0700

    nnn

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,13 @@
  */
 int main(void)
 {
-       	int i, j;
-	
+	int i, j;
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
-			
 			if (i != 8 || j != 9)
 			{
 				putchar(',');
@@ -24,6 +22,5 @@ int main(void)
 		}
         }
 	putchar('\n');
-	
-	return 0;
+	return (0);
 }
