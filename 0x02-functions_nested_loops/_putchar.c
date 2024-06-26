04d361937e4b953cf44ccce83d2ce4b1dda2dc6f
Betty Waiyego <btynyambura20@gmail.com>
2024-06-26 02:36:03 -0700

    cc

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar -writes the charcter c to stdout
+ * &c writes the character to print
+ * Return: returns 1
+ * main - entry point
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
