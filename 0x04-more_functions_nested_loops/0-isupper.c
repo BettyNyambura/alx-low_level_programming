163541b323b485c21c0500ac3a61ae54977ae917
Betty Waiyego <btynyambura20@gmail.com>
2024-06-27 15:01:49 -0700

    comm

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+ * _isupper - checks for uppercase char
+ * @c: Variable
+ * Return: always 0
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
