82a10c7ea0c7e18aea422a10115b55d16a05b8cc
Betty Waiyego <btynyambura20@gmail.com>
2024-06-27 15:18:41 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - a function that checks digits
+ * @a: one digit
+ * @b: another digit
+ * Return: alwats 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
