78384fcb84c82a3d4beae7118d74284b206af268
Betty Waiyego <btynyambura20@gmail.com>
2024-06-27 17:13:20 -0700

    ll

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - funtion to print a triangle
+ * @size: triangle size
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	if (j < size - (i + j))
+	{
+	_putchar(' ');
+	}
+	else
+	{
+	_putchar('#');
+	}
+	}
+	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
