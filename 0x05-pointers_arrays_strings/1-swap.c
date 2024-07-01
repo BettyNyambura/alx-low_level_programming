929c088f4641d5bb9c037a782f7e112c9e9d37af
Betty Waiyego <btynyambura20@gmail.com>
2024-07-01 13:10:31 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two intager values after taking them
+ * @a: swaps and storesaddress of b
+ * @b: swaps and stores address of a
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swp;
+	swp = *b;
+	*b = *a;
+	*a = swp;
+}
