eb30cc6e918364f3cb1b3f56f6275b3f048efef4
Betty Waiyego <btynyambura20@gmail.com>
2024-07-01 12:58:55 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates a pointer to an int
+ * the value pointed to is 98
+ * @n: integer input
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
