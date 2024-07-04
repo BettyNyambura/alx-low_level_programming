2daed4d6acc1b88baaa270b81de8edc6f4134918
Betty Waiyego <btynyambura20@gmail.com>
2024-07-04 15:44:02 -0700

    m

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,10 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-	while (!(str[index] >= 'a' && str[index] <= 'z'))
-	index++;
-	if (str[index - 1] == ' ' ||
+	if (str[index] >= 'a' && str[index] <= 'z')
+	{
+	if (index == 0 ||
+	str[index - 1] == ' ' ||
 	str[index - 1] == ' ' ||
 	str[index - 1] == '\t' ||
 	str[index - 1] == '\n' ||
@@ -27,9 +28,11 @@ char *cap_string(char *str)
 	str[index - 1] == '(' ||
 	str[index - 1] == ')' ||
 	str[index - 1] == '{' ||
-	str[index - 1] == '}' ||
-	index == 0)
+	str[index - 1] == '}')
+	{
 	str[index] -= 32;
+	}
+	}
 	index++;
 	}
 	return (str);
