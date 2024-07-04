0f5aaf3b2ab14cf36acd5906cdcd86acb2d004e6
Betty Waiyego <btynyambura20@gmail.com>
2024-07-04 14:09:10 -0700

    mm

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares pointers
+ * @s1: first pointer
+ * @s2: pointer to the second string
+ * Return: the negative difference
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && s1 == *s2)
+	{
+	s1++;
+	s2++;
+	}
+	return (*s1 - *s2);
+}
