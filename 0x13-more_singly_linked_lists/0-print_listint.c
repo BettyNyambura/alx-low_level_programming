973d1754275db5fdaa5108894d7a05938c04628b
BettyNyambura <btynyambura20@gmail.com>
2024-08-05 13:17:48 -0700

    mm

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,27 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * print_list - Prints all elements of a list
-  * @h: A linked list
-  *
-  * Return: The number of nodes
-  */
-size_t print_list(const list_t *h)
+ * print_listint - print all of a elements in a liked list.
+ * @h: head of the linked list.
+ *
+ * Return: number of nodes of the linked list.
+ */
+
+size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
+		printf("%i\n", h->n);
 		h = h->next;
 		count++;
 	}
-
 	return (count);
 }
