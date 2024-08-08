25dd8136e2c9a9be11c6106b38ee01472907fe6a
BettyNyambura <btynyambura20@gmail.com>
2024-08-08 12:38:09 -0700

    mm

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int max = 0x01;
 
+	if (index >= (sizeof(unsigned long int) * 8))
+	return (-1);
+
 	max <<= index;
-	if (max == 0)
-		return (-1);
 	*n |= max;
 	return (1);
 }
