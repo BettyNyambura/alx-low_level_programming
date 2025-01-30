a358a50930f4d5d88de8bc88ea7a99417b6da138
Betty Waiyego <btynyambura20@gmail.com>
2025-01-30 18:06:47 +0300

    mm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,53 +1,50 @@
 #include "search_algos.h"
-#include <stdio.h>
 
 /**
- * print_array - Prints an array of integers
- * @array: Pointer to the first element of the array
- * @low: The starting index of the array/subarray
- * @high: The ending index of the array/subarray
+ * print_array - print an array
+ * @array: the array to print
+ * @l: the left index
+ * @r: the right index
  */
-
-void print_array(int *array. size_t low, size_t high)
+void print_array(int *array, int l, int r)
 {
-	size_t i;
+	int i;
 
-	for (i = low; i <= high; i++)
+	for (i = l; i <= r; i++)
 	{
-	if (i != low)
-	printf(", ");
-	printf("%d", array[i]);
+		printf("%d", array[i]);
+		if (i < r)
+			printf(", ");
 	}
 	printf("\n");
 }
 
 /**
- * binary_search - Searches for a value in a sorted array using Binary Search
- * @array: Pointer to the first element of the array
- * @size: Number of elements in the array
- * @value: Value to search for
- *
- * Return: The index where value is located, or -1 if not found or NULL
+ * binary_search - search for an element in an array
+ * @array: the array we want to search in
+ * @size: size of the array
+ * @value: the target value
+ * Return: the index of the value or -1 if not found
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high = size - 1, mid;
+	int l = 0, r = size - 1, m;
 
-	if (array == NULL)
-	return (-1);
+	if (!array || size == 0)
+		return (-1);
 
-	while (low <= high)
+	while (l <= r)
 	{
-	mid = low + (high - low) / 2;
-	printf("Searching in array: ");
-	print_array(array, low, high);
+		printf("Searching in array: ");
+		print_array(array, l, r);
+		m = l + (r - l) / 2;
 
-	if (array[mid] == value)
-	return (mid);
-	else if (array[mid] < value)
-	low = mid + 1;
-	else
-	high = mid - 1;
+		if (array[m] > value)
+			r = m - 1;
+		else if (array[m] < value)
+			l = m + 1;
+		else
+			return (m);
 	}
 
 	return (-1);
